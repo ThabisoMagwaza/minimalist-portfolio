3e90668118bdada3c4a8c9d012ad269e7a8020f3
Thabiso Magwaza <thabisomagwaza@capitecbank.co.za>
2024-03-07 11:11:29 +0200

    feat: initial commit

---
+++ src/components/GlobalStyles/GlobalStyles.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  
+  h1 {
+    color: red;
+  }
+
+`;
+
+export default GlobalStyles;
